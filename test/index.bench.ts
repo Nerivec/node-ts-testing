ee6c5b718fa2bdeab9b87f9962d30981db40700d
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-03-21 13:27:11 +0100

    fix formatting

--- test/index.bench.ts
+++ test/index.bench.ts
@@ -2,10 +2,10 @@ import { randomBytes } from "node:crypto";
 import { bench, describe } from "vitest";
 
 describe("Benchmarks", () => {
-	bench("old", async () => {
-		randomBytes(16);
-	});
-	bench("new", async () => {
-		randomBytes(64);
-	});
+    bench("old", async () => {
+        randomBytes(16);
+    });
+    bench("new", async () => {
+        randomBytes(64);
+    });
 });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from "vitest";
 
 describe("Tests", () => {
-	it("1", async () => {
-		expect(1).toStrictEqual(1);
-	});
-	it("2", async () => {
-		expect(2).toStrictEqual(2);
-	});
+    it("1", async () => {
+        expect(1).toStrictEqual(1);
+    });
+    it("2", async () => {
+        expect(2).toStrictEqual(2);
+    });
 });
