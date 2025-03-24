dc54dff6df917ec123caecdc31233083147bb299
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-03-24 14:56:18 +0100

    fix formatting

--- test/index.bench.ts
+++ test/index.bench.ts
@@ -2,10 +2,18 @@ import { randomBytes } from "node:crypto";
 import { bench, describe } from "vitest";
 
 describe("Benchmarks", () => {
-    bench("old", async () => {
-        randomBytes(16);
-    }, {warmupTime: 1000, time: 5000});
-    bench("new", async () => {
-        randomBytes(64);
-    }, {warmupTime: 1000, time: 5000});
+    bench(
+        "old",
+        async () => {
+            randomBytes(16);
+        },
+        { warmupTime: 1000, time: 5000 },
+    );
+    bench(
+        "new",
+        async () => {
+            randomBytes(64);
+        },
+        { warmupTime: 1000, time: 5000 },
+    );
 });
