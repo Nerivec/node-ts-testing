699a3845864f1fe78c84edae53b1cc3bd83f169b
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-03-24 14:55:24 +0100

    increase default timings for bench

--- test/index.bench.ts
+++ test/index.bench.ts
@@ -4,8 +4,8 @@ import { bench, describe } from "vitest";
 describe("Benchmarks", () => {
     bench("old", async () => {
         randomBytes(16);
-    });
+    }, {warmupTime: 1000, time: 5000});
     bench("new", async () => {
         randomBytes(64);
-    });
+    }, {warmupTime: 1000, time: 5000});
 });
